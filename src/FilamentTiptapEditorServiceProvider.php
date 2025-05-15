3b44457e092a77c4c1ad27eac764e69ec8e411b8
Dan Harrin <git@danharrin.com>
2025-05-15 20:30:34 +0100

    [OLYMPUS-572]: Update the Filament TipTap plugin to ensure it registers it's assets with the proper version

--- src/FilamentTiptapEditorServiceProvider.php
+++ src/FilamentTiptapEditorServiceProvider.php
@@ -45,6 +45,6 @@ class FilamentTiptapEditorServiceProvider extends PackageServiceProvider
             $assets[] = Css::make('tiptap-custom-extension-styles', Vite::asset(config('filament-tiptap-editor.extensions_styles')));
         }
 
-        FilamentAsset::register($assets, 'awcodes/tiptap-editor');
+        FilamentAsset::register($assets, 'canyongbs/filament-tiptap-editor');
     }
 }
